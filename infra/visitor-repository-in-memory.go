773a7f7bf1d3228531eee503ed8bf63f1a725a84
Eloy Pérez <ep.falagan@gmail.com>
2025-08-14 16:12:42 +0200

    feat: use new domain value objects in validator domain

--- infra/visitor-repository-in-memory.go
+++ infra/visitor-repository-in-memory.go
@@ -7,26 +7,25 @@ import (
 	"github.com/Falagan/web-tracker/internal/domain"
 )
 
-type UID string
-
 type VisitorRepositoryInMemory struct {
-	visitors map[UID]bool
+	visitors map[domain.UID]bool
 	mu       sync.RWMutex
 }
 
 func NewVisitorRepositoryInMemory() *VisitorRepositoryInMemory {
 	return &VisitorRepositoryInMemory{
-		visitors: make(map[UID]bool),
+		visitors: make(map[domain.UID]bool),
 	}
 }
 
 func (vr *VisitorRepositoryInMemory) AddUnique(ctx context.Context, v *domain.Visitor) error {
 	vr.mu.Lock()
 	defer vr.mu.Unlock()
-	_, exists := vr.visitors[UID(v.UID)]
 
+	_, exists := vr.visitors[v.UID]
 	if !exists {
-		vr.visitors[UID(v.UID)] = true
+		vr.visitors[v.UID] = true
 	}
+
 	return nil
 }
--- internal/domain/visitor.go
+++ internal/domain/visitor.go
@@ -1,10 +1,53 @@
 package domain
 
-import "context"
+import (
+	"context"
+
+	"github.com/Falagan/web-tracker/pkg"
+)
 
 type Visitor struct {
-	UID string `json:"uid"`
-	URL string `json:"url"`
+	UID UID `json:"uid"`
+	URL URL `json:"url"`
+}
+
+var (
+	VisitorInvalidUIDError = pkg.Error{Code: "EDVISITORUID", Message: "Visitor has invalid UID"}
+	VisitorInvalidURLError = pkg.Error{Code: "EDVISITORURL", Message: "Visitor has invalid URL"}
+	VisitorInvalidRequest = pkg.Error{Code: "EDVISITORREQUEST", Message: "Visitor invalid request"}
+)
+
+func NewVisitor(uid string, url string) (*Visitor, error) {
+	validUID, err := NewUID(uid)
+	if err != nil {
+		return nil, err
+	}
+
+	validURL, err := NewURL(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Visitor{
+		UID: validUID,
+		URL: validURL,
+	}, nil
+}
+
+func (v *Visitor) Validate() error {
+	if err := v.UID.Validate(); err != nil {
+		return &VisitorInvalidUIDError
+	}
+
+	if err := v.URL.Validate(); err != nil {
+		return &VisitorInvalidURLError
+	}
+
+	return nil
+}
+
+func (v *Visitor) IsValid() bool {
+	return v.Validate() == nil
 }
 
 type VisitorRepository interface {
