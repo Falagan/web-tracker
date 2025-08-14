9945f867f75335d964a0b890ef8ad34d3547afa3
Eloy Pérez <ep.falagan@gmail.com>
2025-08-14 16:11:54 +0200

    feat: enrich anayltic domain with validations

--- internal/domain/analytic.go
+++ internal/domain/analytic.go
@@ -1,12 +1,49 @@
 package domain
 
-import "context"
+import (
+	"context"
 
-type URL string
-type URLCount int
+	"github.com/Falagan/web-tracker/pkg"
+)
 
 type Analytic struct {
-	data map[URL]URLCount
+	URL   URL
+	Count URLCount
+}
+
+var (
+	AnalyticInvalidURLError   = pkg.Error{Code: "EDANALYTICURL", Message: "Analytic has invalid URL"}
+	AnalyticInvalidCountError = pkg.Error{Code: "EDANALYTICCOUNT", Message: "Analytic has invalid count"}
+)
+
+func NewAnalytic(url string, count int) (*Analytic, error) {
+	validURL, err := NewURL(url)
+	if err != nil {
+		return nil, err
+	}
+	
+	validCount := NewURLCount(count)
+	
+	return &Analytic{
+		URL:   validURL,
+		Count: validCount,
+	}, nil
+}
+
+func (a *Analytic) Validate() error {
+	if err := a.URL.Validate(); err != nil {
+		return &AnalyticInvalidURLError
+	}
+	
+	if !a.Count.IsValid() {
+		return &AnalyticInvalidCountError
+	}
+	
+	return nil
+}
+
+func (a *Analytic) IsValid() bool {
+	return a.Validate() == nil
 }
 
 type AnalyticRepository interface {
